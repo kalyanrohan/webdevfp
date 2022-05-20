1076795da04494365dc706fcf39d0edbe5f99aa3
kalyanrohan <sri.rohan@binus.ac.id>
2022-05-20 16:20:44 +0700

    signup

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,24 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database"
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from  'firebase/compat/app'
+import 'firebase/compat/auth'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDX3fisLO_Ho8pMNzNnNSUsT-_JXPJIEro",
-  authDomain: "good-head.firebaseapp.com",
-  databaseURL: "https://good-head-default-rtdb.firebaseio.com",
-  projectId: "good-head",
-  storageBucket: "good-head.appspot.com",
-  messagingSenderId: "501161084201",
-  appId: "1:501161084201:web:71dacbce0875172b416338",
-  measurementId: "G-MVDR46KMMZ"
-};
+const app= firebase.initializeApp({
+    apiKey: process.env.REACT_API_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_API_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_API_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_API_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_API_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_API_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_API_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_API_FIREBASE_MEASUREMENT_ID
+})
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const database = getDatabase(app);
+export const auth = app.auth()
+export default app
