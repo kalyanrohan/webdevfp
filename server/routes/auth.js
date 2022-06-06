39d016d6d0d5b036d32036c83f02eb53b23e90c8
kalyanrohan <sri.rohan@binus.ac.id>
2022-06-06 22:02:07 +0700

    make it better

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
-const { signup, login, editProfile } = require('../controllers/auth.js');
+const { signup, login } = require('../controllers/auth.js');
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.post('/editProfile', editProfile);
-
 
 module.exports = router;
