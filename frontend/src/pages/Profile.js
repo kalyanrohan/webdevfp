2c10375c9588741cdf570f0b26995bff0a778cf1
Raphael Reynaldi <71363614+relydda20@users.noreply.github.com>
2022-06-13 11:25:31 +0700

    monggodb

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { Col, Container, Form, Row, Button } from "react-bootstrap";
+import { useUpdateUserMutation } from "../services/appApi";
+import {useNavigate } from "react-router-dom";
+import "./Signup.css";
+import botImg from "../assets/bot.jpeg";
+
+function Signup() {
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [updateUser, { isLoading, error }] = useUpdateUserMutation();
+    const navigate = useNavigate();
+    //image upload states
+    const [image, setImage] = useState(null);
+    const [upladingImg, setUploadingImg] = useState(false);
+    const [imagePreview, setImagePreview] = useState(null);
+
+    function validateImg(e) {
+        const file = e.target.files[0];
+        if (file.size >= 1048576) {
+            return alert("Max file size is 1mb");
+        } else {
+            setImage(file);
+            setImagePreview(URL.createObjectURL(file));
+        }
+    }
+
+    async function uploadImage() {
+        const data = new FormData();
+        data.append("file", image);
+        data.append("upload_preset", "GoodHeadUploadSetting");
+        try {
+            setUploadingImg(true);
+            let res = await fetch("https://api.cloudinary.com/v1_1/dqzeq3nyp/image/upload", {
+                method: "post",
+                body: data,
+            });
+            const urlData = await res.json();
+            setUploadingImg(false);
+            return urlData.url;
+        } catch (error) {
+            setUploadingImg(false);
+            console.log(error);
+        }
+    }
+
+    async function handleSignup(e) {
+        e.preventDefault();
+        // signup the user
+        updateUser({ name, password}).then(({ data }) => {
+            if (data) {
+                console.log(data);
+                navigate("/chat");
+            }
+        });
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col md={7} className="d-flex align-items-center justify-content-center flex-direction-column">
+                    <Form style={{ width: "80%", maxWidth: 500 }} onSubmit={handleSignup}>
+                        <h1 className="text-center">Edit Profile</h1>
+                        <div className="signup-profile-pic__container">
+                            <img src={imagePreview || botImg} className="signup-profile-pic" />
+                            <label htmlFor="image-upload" className="image-upload-label">
+                                <i className="fas fa-plus-circle add-picture-icon"></i>
+                            </label>
+                            <input type="file" id="image-upload" hidden accept="image/png, image/jpeg" onChange={validateImg} />
+                        </div>
+                        {error && <p className="alert alert-danger">{error.data}</p>}
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                            <Form.Label>Change Name</Form.Label>
+                            <Form.Control type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} value={name} />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Change Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            {upladingImg || isLoading ? "Signing you up..." : "Signup"}
+                        </Button>
+                    </Form>
+                </Col>
+                <Col md={5} className="signup__bg"></Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Signup;
